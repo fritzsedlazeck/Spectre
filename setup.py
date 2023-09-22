046db8d37b5a34b5fe165d0ee9d9a4c4ba6f5480
Eric T. Dawson <eric.t.dawson@gmail.com>
2023-09-22 11:16:41 -0400

    Add spectre's init.py; tweak setup.py to fix pip install and add descriptors and a version number; add egg info to gitignore.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name="spectre",
-        version="",
-        description="",
+        version="0.1.0",
+        description="Spectre - a CNV caller for long read data",
         author="",
-        url="",
+        url="https://github.com/fritzsedlazeck/Spectre",
         include_package_data=True,
         packages=find_packages(exclude=[]),
         entry_points = {
@@ -15,6 +15,14 @@ setup(name="spectre",
         python_requires='>=3.7',
         install_requires=["pysam>=0.21.0", "numpy>=1.24.3", "pandas>=2.0.1", "matplotlib>=3.7.1", "scipy>=1.10.1"],
         long_description="",
-        classifiers=[],
+        classifiers=[
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Natural Language :: English",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10"
+        ],
         platforms=["any"]
         )
